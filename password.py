f9bfb0193f3b35299b81dfdef29d2ba1e123c914
Nicat1q <143207192+Nicat1q@users.noreply.github.com>
2023-10-05 22:34:53 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+import random
+
+def gen_password(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
